212137c36b862741c8ec36e4f3581822d05a23cf
Julien Custoja <julien.custoja@gmail.com>
2015-01-25 11:21:22 +0100

    Début vue Matière UE

--- app/views/layout/menu.blade.php
+++ app/views/layout/menu.blade.php
@@ -2,74 +2,70 @@
 <!-- Note: This width of the aside area can be adjusted through LESS variables -->
 <aside id="left-panel">
 
-<!-- User info -->
-<div class="login-info">
-				<span> <!-- User image size is adjusted inside CSS, it should stay as it -->
+    <!-- User info -->
+    <div class="login-info">
+        <span> <!-- User image size is adjusted inside CSS, it should stay as it -->
 
-					<a id="show-shortcut" data-action="toggleShortcut">
-                        <img src="{{ asset('img/avatars/sunny.png') }}" alt="me" class="online" />
-						<span>
-                             <?php
-                            // $nom = Session::get('utilisateur')->nom;
-                            // $prenom = Session::get('utilisateur')->prenom;
-                            ?>
-							{{--@if(!empty($nom) && !empty($prenom)) {{$nom}} {{$prenom}} @else {{Session::get('utilisateur')->email}} @endif --}}
-						</span>
-                    </a>
+            <a id="show-shortcut" data-action="toggleShortcut">
+                <img src="{{ asset('img/avatars/sunny.png') }}" alt="me" class="online" />
+                <span>
+                    <?php
+                    // $nom = Session::get('utilisateur')->nom;
+                    // $prenom = Session::get('utilisateur')->prenom;
+                    ?>
+                    {{--@if(!empty($nom) && !empty($prenom)) {{$nom}} {{$prenom}} @else {{Session::get('utilisateur')->email}} @endif --}}
+                </span>
+            </a>
 
-				</span>
-</div>
-<!-- end user info -->
+        </span>
+    </div>
+    <!-- end user info -->
 
-<!-- NAVIGATION : This navigation is also responsive
+    <!-- NAVIGATION : This navigation is also responsive
+    
+    To make this navigation dynamic please make sure to link the node
+    (the reference to the nav > ul) after page load. Or the navigation
+    will not initialize.
+    -->
+    <nav>
+    <!-- NOTE: Notice the gaps after each icon usage <i></i>..
+    Please note that these links work a bit different than
+    traditional href="" links. See documentation for details.
+        -->
 
-To make this navigation dynamic please make sure to link the node
-(the reference to the nav > ul) after page load. Or the navigation
-will not initialize.
--->
-<nav>
-<!-- NOTE: Notice the gaps after each icon usage <i></i>..
-Please note that these links work a bit different than
-traditional href="" links. See documentation for details.
--->
-
-<ul>
-    @if(isset($premiere_connexion) && $premiere_connexion == true)
-    <li><a class="disabled"> <span class="menu-item-parent">Pour activer le menu, merci de remplir le formulaire de première connexion.</span></a></li>
-    @else
-    <li>
-        <a href="{{URL::route('formation')}}" class="" title="Dashboard"><i class="fa fa-lg fa-fw fa-dashboard "></i> <span class="menu-item-parent">Formation</span></a>
-    </li>
-    <li>
-        <a href="{{URL::route('matiere')}}" class=""><i class="fa fa-lg fa-fw fa-road"></i> <span class="menu-item-parent">UEs / Matières</span><span class="badge pull-right inbox-badge">2</span></a>
-    </li>
-    <li>
-        <a href="{{URL::route('enseignant')}}" class=""><i class="fa fa-lg fa-fw fa-automobile "></i> <span class="menu-item-parent">Enseignants</span></a>
-    </li>
-    <li>
-        <a href="{{URL::route('calendrier')}}" title="Exporter"><i class="fa fa-lg fa-fw fa-cloud-download"></i> <span class="menu-item-parent">Calendrier</span></a>
-    </li>
-    <li>
-        <a href="{{URL::route('affectation')}}" title="Journal"><i class="fa fa-lg fa-fw fa-cogs  "></i> <span class="menu-item-parent">Affectation</span></a>
-    </li>
-    <li>
-        <a href="{{URL::route('voeux')}}" title="Adresses"><i class="fa fa-lg fa-fw fa-location-arrow "></i> <span class="menu-item-parent">Voeux</span></a>
-    </li>
-    <li>
-        <a href="{{URL::route('monservice')}}" title="Timeline"><i class="fa fa-lg fa-fw fa-send-o"></i> <span class="menu-item-parent">Mon service</span></a>
-    </li>
-    <li>
-        <a href="{{URL::route('heuresexterieures')}}" title="Timeline"><i class="fa fa-lg fa-fw fa-send-o"></i> <span class="menu-item-parent">Heures exterieures</span></a>
-    </li>
-    <li>
-        <a href="{{URL::route('deconnexion')}}" title="Déconnexion"><i class="fa fa-lg fa-fw fa-external-link-square"></i> <span class="menu-item-parent">Déconnexion</span></a>
-    </li>
-    @endif
-</ul>
-</nav>
-			<span class="minifyme" data-action="minifyMenu">
-				<i class="fa fa-arrow-circle-left hit"></i>
-			</span>
+        <ul>
+            <li>
+                <a href="{{URL::route('formation')}}" class="" title="Formation"><i class="fa fa-lg fa-fw fa-dashboard "></i> <span class="menu-item-parent">Formation</span></a>
+            </li>
+            <li>
+                <a href="{{URL::route('matiere')}}" class=""><i class="fa fa-lg fa-fw fa-road"></i> <span class="menu-item-parent">UEs / Matières</span><span class="badge pull-right inbox-badge">2</span></a>
+            </li>
+            <li>
+                <a href="{{URL::route('enseignant')}}" class=""><i class="fa fa-lg fa-fw fa-automobile "></i> <span class="menu-item-parent">Enseignants</span></a>
+            </li>
+            <li>
+                <a href="{{URL::route('calendrier')}}" title="Calendrier"><i class="fa fa-lg fa-fw fa-cloud-download"></i> <span class="menu-item-parent">Calendrier</span></a>
+            </li>
+            <li>
+                <a href="{{URL::route('affectation')}}" title="Affectation"><i class="fa fa-lg fa-fw fa-cogs  "></i> <span class="menu-item-parent">Affectation / plannification</span></a>
+            </li>
+            <li>
+                <a href="{{URL::route('voeux')}}" title="Adresses"><i class="fa fa-lg fa-fw fa-location-arrow "></i> <span class="menu-item-parent">Voeux</span></a>
+            </li>
+            <li>
+                <a href="{{URL::route('monservice')}}" title="Timeline"><i class="fa fa-lg fa-fw fa-send-o"></i> <span class="menu-item-parent">Mon service</span></a>
+            </li>
+            <li>
+                <a href="{{URL::route('heuresexterieures')}}" title="Timeline"><i class="fa fa-lg fa-fw fa-send-o"></i> <span class="menu-item-parent">Heures exterieures</span></a>
+            </li>
+            <li>
+                <a href="{{URL::route('deconnexion')}}" title="Déconnexion"><i class="fa fa-lg fa-fw fa-external-link-square"></i> <span class="menu-item-parent">Déconnexion</span></a>
+            </li>
+        </ul>
+    </nav>
+    <span class="minifyme" data-action="minifyMenu">
+        <i class="fa fa-arrow-circle-left hit"></i>
+    </span>
 
 </aside>
 <!-- END NAVIGATION -->
--- app/views/matiere.blade.php
+++ app/views/matiere.blade.php
@@ -4,56 +4,209 @@
         <div class="row">
             <!-- NEW WIDGET START -->
             <h1>Formation / UE / Matières <small>Cette page permet de gérer le volume de CM/TD/TP</small></h1>
+            <br/>
+            <br/>
             <!-- NEW WIDGET START -->
-            <article class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-
+            <article class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                 <!-- Widget ID (each widget will need unique ID)-->
-                <div class="jarviswidget jarviswidget-color-darken" id="wid-id-2" data-widget-editbutton="false" data-widget-colorbutton="false" data-widget-deletebutton="false" data-widget-fullscreenbutton="false">
-                    <!-- widget options:
-                            usage: <div class="jarviswidget" id="wid-id-0" data-widget-editbutton="false">
-    
-                            data-widget-colorbutton="false"
-                            data-widget-editbutton="false"
-                            data-widget-togglebutton="false"
-                            data-widget-deletebutton="false"
-                            data-widget-fullscreenbutton="false"
-                            data-widget-custombutton="false"
-                            data-widget-collapsed="true"
-                            data-widget-sortable="false"
-    
-                    -->
+                <div class="jarviswidget jarviswidget-color-darken" id="formation-1" 
+                     data-widget-editbutton="false" 
+                     data-widget-colorbutton="false" 
+                     data-widget-deletebutton="false" 
+                     data-widget-fullscreenbutton="false" 
+                     data-widget-collapsed="true">
+
                     <header>
                         <span class="widget-icon"> <i class="fa fa-arrows-v"></i> </span>
-                        <h2 class="font-md"><strong>Collapsable</strong> <i>Widget</i></h2>
-
+                        <h2 class="font-md"><strong>D.U.T</strong> <i>Première année</i></h2>
                     </header>
 
                     <!-- widget div-->
                     <div>
-
-                        <!-- widget edit box -->
                         <div class="jarviswidget-editbox">
                             <!-- This area used as dropdown edit box -->
-
                         </div>
                         <!-- end widget edit box -->
-
                         <!-- widget content -->
                         <div class="widget-body">
-
-                            <h2 class="alert alert-warning"> This widget is opened by default </h2>
-                            <code>data-widget-togglebutton</code>
-                            <br><br>
-                            <p>Notice the header text size slightly larger than other widget headers</p>
-
+                            <!-- ICI CODE DES UE-->
+                            <p>DUT PREMIERE ANNEE</p>
                         </div>
                         <!-- end widget content -->
-
                     </div>
                     <!-- end widget div -->
-
                 </div>
                 <!-- end widget -->
+                <!-- Widget ID (each widget will need unique ID)-->
+                <div class="jarviswidget jarviswidget-color-darken" id="formation-2" 
+                     data-widget-editbutton="false" 
+                     data-widget-colorbutton="false" 
+                     data-widget-deletebutton="false" 
+                     data-widget-fullscreenbutton="false" 
+                     data-widget-collapsed="true">
+
+                    <header>
+                        <span class="widget-icon"> <i class="fa fa-arrows-v"></i> </span>
+                        <h2 class="font-md"><strong>D.U.T</strong> <i>Deuxième année</i></h2>
+                    </header>
+
+                    <!-- widget div-->
+                    <div>
+                        <div class="jarviswidget-editbox">
+                            <!-- This area used as dropdown edit box -->
+                        </div>
+                        <!-- end widget edit box -->
+                        <!-- widget content -->
+                        <div class="widget-body">
+                            <!-- ICI CODE DES UE-->
+                            <p>DUT DEUXIEME ANNEE</p>
+                        </div>
+                        <!-- end widget content -->
+                    </div>
+                    <!-- end widget div -->
+                </div>
+                <!-- end widget -->
+                <!-- Widget ID (each widget will need unique ID)-->
+                <div class="jarviswidget jarviswidget-color-greenLight" id="formation-3" 
+                     data-widget-editbutton="false" 
+                     data-widget-colorbutton="false" 
+                     data-widget-deletebutton="false" 
+                     data-widget-fullscreenbutton="false" 
+                     data-widget-collapsed="true">
+
+                    <header>
+                        <span class="widget-icon"> <i class="fa fa-arrows-v"></i> </span>
+                        <h2 class="font-md"><strong>D.U.T</strong> <i>Année spéciale</i></h2>
+                    </header>
+
+                    <!-- widget div-->
+                    <div>
+                        <div class="jarviswidget-editbox">
+                            <!-- This area used as dropdown edit box -->
+                        </div>
+                        <!-- end widget edit box -->
+                        <!-- widget content -->
+                        <div class="widget-body">
+                            <!-- ICI CODE DES UE-->
+                        </div>
+                        <!-- end widget content -->
+                    </div>
+                    <!-- end widget div -->
+                </div>
+                <!-- end widget -->
+                <!-- Widget ID (each widget will need unique ID)-->
+                <div class="jarviswidget jarviswidget-color-blue" id="formation-4" 
+                     data-widget-editbutton="false" 
+                     data-widget-colorbutton="false" 
+                     data-widget-deletebutton="false" 
+                     data-widget-fullscreenbutton="false" 
+                     data-widget-collapsed="true">
+
+                    <header>
+                        <span class="widget-icon"> <i class="fa fa-arrows-v"></i> </span>
+                        <h2 class="font-md"><strong>D.U.T</strong> <i>Cours du soir</i></h2>
+                    </header>
+
+                    <!-- widget div-->
+                    <div>
+                        <div class="jarviswidget-editbox">
+                            <!-- This area used as dropdown edit box -->
+                        </div>
+                        <!-- end widget edit box -->
+                        <!-- widget content -->
+                        <div class="widget-body">
+                            <!-- ICI CODE DES UE-->
+                        </div>
+                        <!-- end widget content -->
+                    </div>
+                    <!-- end widget div -->
+                </div>
+                <!-- end widget -->
+                <!-- Widget ID (each widget will need unique ID)-->
+                <div class="jarviswidget jarviswidget-color-purple" id="formation-5" 
+                     data-widget-editbutton="false" 
+                     data-widget-colorbutton="false" 
+                     data-widget-deletebutton="false" 
+                     data-widget-fullscreenbutton="false" 
+                     data-widget-collapsed="true">
+
+                    <header>
+                        <span class="widget-icon"> <i class="fa fa-arrows-v"></i> </span>
+                        <h2 class="font-md"><strong>D.U.T</strong> <i>Cours à distance</i></h2>
+                    </header>
+
+                    <!-- widget div-->
+                    <div>
+                        <div class="jarviswidget-editbox">
+                            <!-- This area used as dropdown edit box -->
+                        </div>
+                        <!-- end widget edit box -->
+                        <!-- widget content -->
+                        <div class="widget-body">
+                            <!-- ICI CODE DES UE-->
+                        </div>
+                        <!-- end widget content -->
+                    </div>
+                    <!-- end widget div -->
+                </div>
+                <!-- end widget -->
+                <!-- Widget ID (each widget will need unique ID)-->
+                <div class="jarviswidget jarviswidget-color-red" id="formation-6" 
+                     data-widget-editbutton="false" 
+                     data-widget-colorbutton="false" 
+                     data-widget-deletebutton="false" 
+                     data-widget-fullscreenbutton="false" 
+                     data-widget-collapsed="true">
+
+                    <header>
+                        <span class="widget-icon"> <i class="fa fa-arrows-v"></i> </span>
+                        <h2 class="font-md"><strong>Licence professionel</strong> <i>Licence #1</i></h2>
+                    </header>
+
+                    <!-- widget div-->
+                    <div>
+                        <div class="jarviswidget-editbox">
+                            <!-- This area used as dropdown edit box -->
+                        </div>
+                        <!-- end widget edit box -->
+                        <!-- widget content -->
+                        <div class="widget-body">
+                            <!-- ICI CODE DES UE-->
+                        </div>
+                        <!-- end widget content -->
+                    </div>
+                    <!-- end widget div -->
+                </div>
+                <!-- end widget -->
+                <!-- Widget ID (each widget will need unique ID)-->
+                <div class="jarviswidget jarviswidget-color-red" id="formation-7" 
+                     data-widget-editbutton="false" 
+                     data-widget-colorbutton="false" 
+                     data-widget-deletebutton="false" 
+                     data-widget-fullscreenbutton="false" 
+                     data-widget-collapsed="true">
+
+                    <header>
+                        <span class="widget-icon"> <i class="fa fa-arrows-v"></i> </span>
+                        <h2 class="font-md"><strong>Licence professionel</strong> <i>Licence #2</i></h2>
+                    </header>
+
+                    <!-- widget div-->
+                    <div>
+                        <div class="jarviswidget-editbox">
+                            <!-- This area used as dropdown edit box -->
+                        </div>
+                        <!-- end widget edit box -->
+                        <!-- widget content -->
+                        <div class="widget-body">
+                            <!-- ICI CODE DES UE-->
+                        </div>
+                        <!-- end widget content -->
+                    </div>
+                    <!-- end widget div -->
+                </div>
+                <!-- end widget -->
+            </article>
         </div>
     </section>
 </div>
