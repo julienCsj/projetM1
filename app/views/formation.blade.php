ef0e13df6f2e683905b2a2e994ac7fadff0026fc
Elliot Felgines <felgines.elliot@gmail.com>
2015-01-25 12:03:46 +0100

    Suppression scripts inutiles

--- app/views/formation.blade.php
+++ app/views/formation.blade.php
@@ -1,4 +1,5 @@
 @include('layout.header')
+
 <section id="widget-grid" class="">
     <div class="row">
         <!-- NEW WIDGET START -->
@@ -96,14 +97,6 @@
 </section>
 
 @include('layout.footer')
-<!-- PAGE RELATED PLUGIN(S) -->
-<script src="back/js/plugin/bootstrap-wizard/jquery.bootstrap.wizard.min.js"></script>
-<script src="back/js/plugin/fuelux/wizard/wizard.min.js"></script>
-<script src="back/js/plugin/ion-slider/ion.rangeSlider.min.js"></script>
-<script src="http://maps.google.com/maps/api/js?sensor=false"></script>
-<script src="back/js/plugin/jquery.ui.addresspicker.js"></script>
-
-
 
 <script type="text/javascript">
     // DO NOT REMOVE : GLOBAL FUNCTIONS!
