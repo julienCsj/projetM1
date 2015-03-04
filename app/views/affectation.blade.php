a4b875e1f73d13a8c7d08bb9844dcfbdff693dc0
superkebab <felgines.elliot@gmail.com>
2015-03-04 11:23:13 +0100

    Vue affectation V0.8

--- app/views/affectation.blade.php
+++ app/views/affectation.blade.php
@@ -7,72 +7,55 @@
     </div>
     <div class="row" id="drag">
     	<div class="col-sm-12 col-md-3 col-lg-3">
-    		<div class="jarviswidget jarviswidget-color-blueDark" role="widget">
-				<header>
-					<h2>Cours</h2>
-				</header>
-				<div class="well well-sm" id="event-container">
-					<form>
-						<fieldset>
-							<ul id="external-events" class="list-unstyled">
-	                            <li style="position: relative;">
-	                                <span id="COURS1" class="draggable bg-color-green txt-color-white external-event" data-icon="fa-pie">Cours#1</span>
-	                            </li>
-	                            <li style="position: relative;">
-	                                <span id="COURS2" class="draggable bg-color-green txt-color-white external-event" data-icon="fa-pie">Cours#2</span>
-	                            </li>
-							</ul>
-							<ul id="external-events" class="list-unstyled">
-	                            <li style="position: relative;">
-	                                <span id="TD1" class="draggable bg-color-blue txt-color-white external-event" data-icon="fa-pie">TD#1</span>
-	                            </li>
-	                            <li style="position: relative;">
-	                                <span id="TD2" class="draggable bg-color-blue txt-color-white external-event" data-icon="fa-pie">TD#2</span>
-	                            </li>
-							</ul>
-							<ul id="external-events" class="list-unstyled">
-	                            <li style="position: relative;">
-	                                <span id="TP1" class="draggable bg-color-red txt-color-white external-event" data-icon="fa-pie">TP#1</span>
-	                            </li>
-	                            <li style="position: relative;">
-	                                <span id="TP2" class="draggable bg-color-red txt-color-white external-event" data-icon="fa-pie">TP#2</span>
-	                            </li>
-							</ul>
-						</fieldset>
-					</form>
-				</div>
-			</div>
+			<form>
+				<ul id="external-events" class="list-unstyled">
+                    <li style="position: relative;">
+                        <span id="COURS1" class="draggable bg-color-green txt-color-white external-event" data-icon="fa-pie">Cours#1</span>
+                    </li>
+                    <li style="position: relative;">
+                        <span id="COURS2" class="draggable bg-color-green txt-color-white external-event" data-icon="fa-pie">Cours#2</span>
+                    </li>
+				</ul>
+				<ul id="external-events" class="list-unstyled">
+                    <li style="position: relative;">
+                        <span id="TD1" class="draggable bg-color-blue txt-color-white external-event" data-icon="fa-pie">TD#1</span>
+                    </li>
+                    <li style="position: relative;">
+                        <span id="TD2" class="draggable bg-color-blue txt-color-white external-event" data-icon="fa-pie">TD#2</span>
+                    </li>
+				</ul>
+				<ul id="external-events" class="list-unstyled">
+                    <li style="position: relative;">
+                        <span id="TP1" class="draggable bg-color-red txt-color-white external-event" data-icon="fa-pie">TP#1</span>
+                    </li>
+                    <li style="position: relative;">
+                        <span id="TP2" class="draggable bg-color-red txt-color-white external-event" data-icon="fa-pie">TP#2</span>
+                    </li>
+				</ul>
+			</form>
 		</div>
 
 		<div id="periodes" class="col-sm-12 col-md-9 col-lg-9">
-			<!-- new widget -->
-			<div class="jarviswidget jarviswidget-color-blueDark" role="widget">
-        		<header>
-					<h2>Périodes</h2>
-				</header>
-				<div class="well well-sm" id="event-container">
-					<div class="row">
-						<div class="col-sm-12">
-							<span>Période du x/x/x au x/x/x : x semaines</span>
-						</div>
-						<div class="col-sm-12">
-							<div class="droppable col-sm-3 well">Semaine</div>
-							<div class="droppable col-sm-3 well">Semaine</div>
-							<div class="droppable col-sm-3 well">Semaine</div>
-							<div class="droppable col-sm-3 well">Semaine</div>
-						</div>
-						<div class="col-sm-12">
-							<span>Période du x/x/x au x/x/x : x semaines</span>
-						</div>
-						<div class="col-sm-12">
-							<div class="droppable col-sm-3 well">Semaine</div>
-							<div class="droppable col-sm-3 well">Semaine</div>
-							<div class="droppable col-sm-3 well">Semaine</div>
-							<div class="droppable col-sm-3 well">Semaine</div>
-							<div class="droppable col-sm-3 well">Semaine</div>
-							<div class="droppable col-sm-3 well">Semaine</div>
-						</div>
-					</div>
+			<div class="row">
+				<div class="col-sm-12">
+					<span>Période du x/x/x au x/x/x : x semaines</span>
+				</div>
+				<div class="col-sm-12">
+					<div class="droppable col-sm-3 well">Semaine</div>
+					<div class="droppable col-sm-3 well">Semaine</div>
+					<div class="droppable col-sm-3 well">Semaine</div>
+					<div class="droppable col-sm-3 well">Semaine</div>
+				</div>
+				<div class="col-sm-12">
+					<span>Période du x/x/x au x/x/x : x semaines</span>
+				</div>
+				<div class="col-sm-12">
+					<div class="droppable col-sm-3 well">Semaine</div>
+					<div class="droppable col-sm-3 well">Semaine</div>
+					<div class="droppable col-sm-3 well">Semaine</div>
+					<div class="droppable col-sm-3 well">Semaine</div>
+					<div class="droppable col-sm-3 well">Semaine</div>
+					<div class="droppable col-sm-3 well">Semaine</div>
 				</div>
 			</div>
 		</div>
@@ -81,10 +64,7 @@
 
 <!-- #dialog-message -->
 <div id="dialog-message" title="Affecter">
-    <p>
-        Vous pouvez changer le statut de l'enseignant pour définir le volume horaire, ou le renseigner à la main
-    </p>
-
+    <span>Vous pouvez changer le statut de l'enseignant pour définir le volume horaire, ou le renseigner à la main</span>
     <form id="form-status-enseignant" class="smart-form" novalidate="novalidate">
 
         <fieldset>
@@ -134,11 +114,11 @@
                         $(this).dialog("close");
                     }
                 }, {
-                    html: "<i class='fa fa-check'></i>&nbsp; OK",
+                    html: "<i class='fa fa-check'></i>&nbsp; Valider",
                     "class": "btn btn-primary",
                     click: function () {
                         $(this).dialog("close");
-                        //modifier_statut();
+                        valider_affectation();
                     }
                 }]
         });
@@ -149,26 +129,23 @@
     function init() {
 	    $('.draggable').draggable({
 	    	containment: '#drag',
+	    	stack: '.droppable',
 	    	cursor: 'move',
-	    	zIndex: 100,
 	    	revert: 'invalid',
 		    helper: 'original'
 		});
 		$('.droppable').droppable({
 			drop: handleDropEvent,
-			hoverClass: "hover",
+			activeClass: "well-light",
+			hoverClass: "bg-color-teal",
     		accept: ".draggable"
 		});
 	}
 
-	function myHelper( event ) {
-		return '<span class="bg-color-green txt-color-white external-event" data-icon="fa-pie">Cours</span>';
-	}
-
-	function handleDropEvent( event, ui ) {
+	function handleDropEvent(event, ui){
 		var draggable = ui.draggable;
 		$('#dialog-message').dialog('open');
-		//var droppable = ui.droppable
+		draggable.data("draggable").originalPosition = {top:0, left:0};
 		//alert( 'Le cours ayant pour id "' + draggable.attr('id') + '" est tombé sur oim, "'+droppable.attr('id')+'" !' );
 	}
 </script>
