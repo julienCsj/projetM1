c6d61590dd7694c7de63818cbefeda39d7b31a57
superkebab <felgines.elliot@gmail.com>
2015-03-30 09:19:34 +0200

    Revert "Planif : correction planification decalage"
    This reverts commit e645503f0f50cee2e220da9652d8e8358574a1f4.

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'scolarite_nil',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'root',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
