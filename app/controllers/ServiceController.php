4b79475bf2e3e4632332047e310466fa39684f7f
Serut <l.mieulet@gmail.com>
2015-03-30 09:29:33 +0200

    Récupère le bon user id pour calculer le service

--- app/controllers/ServiceController.php
+++ app/controllers/ServiceController.php
@@ -3,12 +3,11 @@ class ServiceController extends BaseController {
     public function getService()
     {
         //setlocale(LC_ALL, 'fr_FR');
-
         //exit(var_dump(CalculerChargeService::calculerServiceEnseignantParSemaine(Session::get("user")->id)));
         $data = array(
             'notifications' => array(),
             'breadcrumb' => array('#ApplicationJaneDoe', 'le service'),
-            'service' => CalculerChargeService::calculerServiceEnseignantParSemaine(Session::get("user")->id),
+            'service' => CalculerChargeService::calculerServiceEnseignantParSemaine(Session::get("user")->LOGIN),
         );
         return View::make('service')->with($data);
     }
