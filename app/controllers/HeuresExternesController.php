38172a5ea7f1364206e600b92284e0a3a54246ef
Julien Custoja <julien.custoja@gmail.com>
2015-04-29 10:48:59 +0200

    Heure complémentaires rsp

--- app/controllers/HeuresExternesController.php
+++ app/controllers/HeuresExternesController.php
@@ -26,8 +26,8 @@ class HeuresExternesController extends BaseController {
         $heure->enseignantID = Input::get("enseignantID");
         $heure->save();
 
-        if(Input::get("fromFiche") == "true") {
-            return Redirect::action('GenerationFicheController@getFiche')->with(array("idEnseignant", $heure->enseignantID));
+        if(Input::get("fromFiche")) {
+            return Redirect::action('GenerationFicheController@getFiche')->with(array("idEnseignant" => $heure->enseignantID));
         }
         return Redirect::route('heuresexterieures');
     }
